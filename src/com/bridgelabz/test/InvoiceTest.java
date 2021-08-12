c6440182bc8977b1d29ea9ee526e4b89fb0732e6
23Shivani <shivani.gharve.2310@gmail.com>
2021-08-12 23:16:53 +0530

    Added: Invoice Summary Generation for Multiple Rides Program

--- src/com/bridgelabz/test/InvoiceTest.java
+++ src/com/bridgelabz/test/InvoiceTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import com.bridgelabz.main.InvoiceGenerator;
+import com.bridgelabz.main.InvoiceSummary;
 import com.bridgelabz.main.Ride;
 
 public class InvoiceTest {
@@ -54,5 +55,28 @@ InvoiceGenerator invoiceGenerator;
 		double fare = invoiceGenerator.calculateFare(ride);
 		assertEquals(571, fare,  0.0);		
 	}
-
+	
+	@Test
+	public void givenMultipleRides_shouldReturn_invoiceSummary() {
+		Ride [] ride = {
+				        new Ride(20.0, 15),
+				        new Ride(0.3, 2),
+				        new Ride(5.0, 3)
+		};
+		InvoiceSummary invoiceSummary = invoiceGenerator.calculateTotalFare(ride);
+		InvoiceSummary expected = new InvoiceSummary(3, 273, 91);
+		assertEquals(expected, invoiceSummary);		
+	}
+	
+	@Test
+	public void givenMultipleRides_shouldReturn_InvoiceSummary() {
+		Ride [] ride = {
+				        new Ride(2.0, 14),
+				        new Ride(0.5, 1),
+				        new Ride(50.0, 30)
+		};
+		InvoiceSummary invoiceSummary = invoiceGenerator.calculateTotalFare(ride);
+		InvoiceSummary expected = new InvoiceSummary(3, 570, 190);
+		assertEquals(expected, invoiceSummary);		
+	}
 }
